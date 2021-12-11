559e96d833ab7de43fd5d200d3ed619c65ab1e0f
Arslan Abdullaev <ars352@yahoo.com>
2021-12-11 20:45:58 +0300

    after hw check changes

---
+++ viewBackgroundChange/ViewControllerSecond.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerSecond.swift
+//  viewBackgroundChange
+//
+//  Created by Arslan Abdullaev on 11.12.2021.
+//
+
+import UIKit
+
+class ViewControllerSecond: UIViewController {
+
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var changeView: UIView!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+    
+    
+
+}
