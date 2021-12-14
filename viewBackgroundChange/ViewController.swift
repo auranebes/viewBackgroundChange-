edb8abded5e66a9989776485c0b74c52d7bb14ba
Arslan Abdullaev <ars352@yahoo.com>
2021-12-14 19:25:45 +0300

    after lesson changes

--- viewBackgroundChange/ViewController.swift
+++ viewBackgroundChange/ViewController.swift
@@ -74,6 +74,14 @@ class ViewController: UIViewController {
         globalBlue = CGFloat(blueSlider.value)
         viewForColorChange.backgroundColor = UIColor(red: globalRed ?? 0.1, green: globalGreen ?? 0.1, blue: blueSliderValue, alpha: blueSliderValue)
     }
+    
+    
+    @IBAction func takeAnotherScreen() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "SecondViewController")
+        self.present(viewController, animated: true, completion: nil)
+    }
+    
 }
 
 /*
--- viewBackgroundChange/ViewControllerSecond.swift
+++ viewBackgroundChange/ViewControllerSecond.swift
@@ -12,18 +12,68 @@ class ViewControllerSecond: UIViewController {
     @IBOutlet weak var backView: UIView!
     @IBOutlet weak var changeView: UIView!
     
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     
-    
-    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        changeView.layer.cornerRadius = 15
+        backView.layer.cornerRadius = 7
+        
+        setColor()
+        setValue(for: redLabel, greenLabel, blueLabel)
 
     }
     
 
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+        
+        switch sender {
+        case redSlider: setValue(for: redLabel)
+        case greenSlider: setValue(for: greenLabel)
+        default: setValue(for: blueLabel)
+        }
+        
+    }
     
     
+    private func setColor() {
+        changeView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
     
+    private func setValue(for labels: UILabel...) {
+        
+        labels.forEach { label in
+            switch label {
+            case redLabel:
+                label.text = string(from: redSlider)
+            case greenLabel:
+                label.text = string(from: greenSlider)
+            default:
+                label.text = string(from: blueSlider)
+            }
+        }
+    }
+        
+    private func string(from slider: UISlider) -> String {
+            String(format: "%.2f", slider.value)
+        }
+        
+    @IBAction func takeAnother() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    }
 
-}
+
